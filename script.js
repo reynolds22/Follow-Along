24da68e9b6156bbac81d3db2852b551bb8c7221d
reynolds22 <zacharysreynolds1830@gmail.com>
2024-03-06 21:15:44 -0500

    got done with most of the html, css and js. still have to update the img's and making it responsive.

---
+++ script.js
@@ -0,0 +1,22 @@
+const apiKey = "5f403f01f3ef08df297c9d97efb80085";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=imperial&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°F";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " mph";
+
+}
+
+searchBtn.addEventListener('click', ()=>{
+    checkWeather(searchBox.value);
+});
